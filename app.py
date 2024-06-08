efa18f89905d3e34dca78ccbb8f516a0f75db07a
LeoTeodoro <leoteodoro18@gmail.com>
2024-06-08 15:12:38 -0300

    frontend

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-import numpy as np
+from flask import Flask, render_template, request
 import pandas as pd
+import numpy as np
 
-arr = np.array([-80,-70,-100,-16,0,0,0,0,
-                1,1,1,4,1,0,0,250,
-                0,1,1,2,0,1,0,600,
-                3,2,4,0,0,0,1,500])
-arr = arr.reshape(4,8)
+app = Flask(__name__)
 
-df = pd.DataFrame(columns=['E','M','A','P','X1','X2','X3','LD'],
-                  index=['Z','X1','X2','X3'],
-                  data=arr)
-
+# Funções do Simplex
 def encontrar_coluna_pivo(dataframe):
     first_row = dataframe.iloc[0]
-
-    column_most_negative =first_row.astype(float).idxmin()
-
+    column_most_negative = first_row.astype(float).idxmin()
     return column_most_negative
 
 def encontrar_linha_pivo(dataframe, column):
@@ -40,15 +32,14 @@ def encontrar_linha_pivo(dataframe, column):
 def new_board(dataframe, row, column):
     element = dataframe.loc[row, column]
     new_df = dataframe.astype(float)
-    new_df.loc[row] = new_df.loc[row]/element
+    new_df.loc[row] = new_df.loc[row] / element
     for i in new_df.index:
         if i != row:
-            new_df.loc[i] = new_df.loc[i] - new_df.loc[i][column]*new_df.loc[row]
+            new_df.loc[i] = new_df.loc[i] - new_df.loc[i][column] * new_df.loc[row]
     return new_df
 
 def atualizar_board(dataframe, row, column):
     dataframe.rename(columns={column: row}, index={row: column}, inplace=True)
-
     return dataframe
 
 def veificar_parada(dataframe):
@@ -57,7 +48,7 @@ def veificar_parada(dataframe):
         return True
     else:
         return False
-
+    
 def simplex(dataframe):
     while not veificar_parada(dataframe):
         cpivo = encontrar_coluna_pivo(dataframe)
@@ -66,4 +57,31 @@ def simplex(dataframe):
         dataframe = atualizar_board(newdf, lpivo, cpivo)
     return dataframe
 
-print(simplex(df))
+# Rota para a página inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Rota para processar os dados do formulário
+@app.route('/process', methods=['POST'])
+def process():
+    columns = request.form.getlist('columns[]')
+    rows = request.form.getlist('rows[]')
+    values = request.form.getlist('values[]')
+
+    # Reshape dos valores para uma matriz 4x8
+    values_matrix = np.array(values, dtype=float).reshape((4, 8))
+
+    # Criação do DataFrame
+    df = pd.DataFrame(values_matrix, columns=columns, index=rows)
+
+    # Aplicação do método Simplex
+    result_df = simplex(df)
+
+    # Conversão do DataFrame resultante para HTML
+    result_html = result_df.to_html(classes='table table-bordered')
+
+    return render_template('result.html', result=result_html)
+
+if __name__ == '__main__':
+    app.run(debug=True)
